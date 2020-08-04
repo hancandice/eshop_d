743917d02a8ec8b83739cc07b1123293041a2e66
Candice Han <jeeyoung.han82@gmail.com>
2020-08-04 17:06:01 +0200

    Change Settings

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -1,11 +1,11 @@
 import os
 import dotenv
 
-
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
